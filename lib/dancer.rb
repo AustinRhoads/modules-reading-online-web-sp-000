768adbeb3e15d2bcceef18671b7cfd3e99ac609d
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-03 22:03:34 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,9 +3,9 @@ require_relative './dance_module.rb'
 
 class Dancer
 
-attr_accessor :name 
+attr_accessor :name
 
-def initialize (name) 
-  @name = name 
+def initialize (name)
+  @name = name
 end
 end
